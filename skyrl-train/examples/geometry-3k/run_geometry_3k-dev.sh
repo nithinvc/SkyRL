c145e359c627b5074bb3d25980a9055e6b1f7934
Nithin Chalapathi <nithinc@berkeley.edu>
2026-02-06 02:23:25 +0000

    update env + add updated runners

--- skyrl-train/examples/geometry-3k/run_geometry_3k-dev.sh
+++ skyrl-train/examples/geometry-3k/run_geometry_3k-dev.sh
@@ -17,8 +17,8 @@ set -x
 # : "${LOGGER:=wandb}"
 : "${INFERENCE_BACKEND:=vllm}"
 
-uv run --isolated --extra $INFERENCE_BACKEND --extra ipdb -m skyrl_train.entrypoints.main_base \
-  data.train_data="['$DATA_DIR/train-dev.parquet']" \
+uv run --isolated --extra $INFERENCE_BACKEND -m skyrl_train.entrypoints.main_base \
+  data.train_data="['$DATA_DIR/train.parquet']" \
   data.val_data="['$DATA_DIR/test.parquet']" \
   trainer.algorithm.advantage_estimator="grpo" \
   trainer.policy.model.path="Qwen/Qwen3-VL-2B-Instruct" \
@@ -34,14 +34,14 @@ uv run --isolated --extra $INFERENCE_BACKEND --extra ipdb -m skyrl_train.entrypo
   trainer.eval_before_train=true \
   trainer.eval_interval=2 \
   trainer.update_epochs_per_batch=1 \
-  trainer.train_batch_size=64 \
-  trainer.policy_mini_batch_size=32 \
-  trainer.micro_forward_batch_size_per_gpu=4 \
-  trainer.micro_train_batch_size_per_gpu=4 \
-  trainer.use_sample_packing=false \
+  trainer.train_batch_size=8 \
+  trainer.policy_mini_batch_size=8 \
+  trainer.micro_forward_batch_size_per_gpu=2 \
+  trainer.micro_train_batch_size_per_gpu=2 \
+  trainer.use_sample_packing=true \
   trainer.ckpt_interval=5 \
   trainer.max_prompt_length=1024 \
-  generator.sampling_params.max_generate_length=1024 \
+  generator.sampling_params.max_generate_length=512 \
   trainer.policy.optimizer_config.lr=1.0e-6 \
   trainer.algorithm.use_kl_loss=true \
   generator.backend=$INFERENCE_BACKEND \
--- skyrl-train/examples/geometry-3k/run_geometry_3k.sh
+++ skyrl-train/examples/geometry-3k/run_geometry_3k.sh
@@ -12,16 +12,29 @@ set -x
 # You can override defaults with environment variables:
 #   NUM_GPUS=4 DATA_DIR=/path/to/data bash examples/geometry-3k/run_geometry_3k.sh
 
+# tested on 8xH100 80GB
+
 : "${DATA_DIR:="$HOME/data/geometry_3k"}"
-: "${NUM_GPUS:=4}"
-: "${LOGGER:=console}"  # change to "wandb" for W&B logging
+: "${NUM_GPUS:=8}"
+: "${LOGGER:=wandb}"  # change to "wandb" for W&B logging
 : "${INFERENCE_BACKEND:=vllm}"
+
+# args from slime
+# grpo_args = (
+#     "--advantage-estimator grpo "
+#     "--kl-loss-coef 0.00 "
+#     "--kl-loss-type low_var_kl "
+#     "--kl-coef 0.00 "
+#     "--entropy-coef 0.00 "
+#     "--eps-clip 0.2 "
+#     "--eps-clip-high 0.28 "
+# )
 
 uv run --isolated --extra $INFERENCE_BACKEND -m skyrl_train.entrypoints.main_base \
   data.train_data="['$DATA_DIR/train.parquet']" \
-  data.val_data="['$DATA_DIR/train-dev.parquet']" \
+  data.val_data="['$DATA_DIR/test.parquet']" \
   trainer.algorithm.advantage_estimator="grpo" \
-  trainer.policy.model.path="Qwen/Qwen3-VL-2B-Instruct" \
+  trainer.policy.model.path="Qwen/Qwen2.5-VL-3B-Instruct" \
   trainer.placement.colocate_all=true \
   trainer.strategy=fsdp2 \
   trainer.placement.policy_num_gpus_per_node=$NUM_GPUS \
@@ -34,26 +47,29 @@ uv run --isolated --extra $INFERENCE_BACKEND -m skyrl_train.entrypoints.main_bas
   trainer.eval_before_train=true \
   trainer.eval_interval=5 \
   trainer.update_epochs_per_batch=1 \
-  trainer.train_batch_size=512 \
-  trainer.policy_mini_batch_size=128 \
-  trainer.micro_forward_batch_size_per_gpu=32 \
-  trainer.micro_train_batch_size_per_gpu=32 \
+  trainer.train_batch_size=64 \
+  trainer.policy_mini_batch_size=64 \
+  trainer.micro_forward_batch_size_per_gpu=16 \
+  trainer.micro_train_batch_size_per_gpu=8 \
   trainer.ckpt_interval=10 \
   trainer.max_prompt_length=1024 \
   generator.sampling_params.max_generate_length=1024 \
   trainer.policy.optimizer_config.lr=1.0e-6 \
+  trainer.policy.optimizer_config.weight_decay=0.1 \
+  trainer.policy.optimizer_config.adam_betas=[0.9,0.98] \
   trainer.algorithm.use_kl_loss=true \
+  trainer.algorithm.kl_loss_coef=1e-2 \
   generator.backend=$INFERENCE_BACKEND \
   generator.run_engines_locally=true \
   generator.weight_sync_backend=nccl \
   generator.async_engine=true \
   generator.batched=true \
   environment.env_class=geometry-3k \
-  generator.n_samples_per_prompt=5 \
+  generator.n_samples_per_prompt=8 \
   generator.gpu_memory_utilization=0.8 \
   trainer.logger="$LOGGER" \
   trainer.project_name="geometry-3k" \
-  trainer.run_name="geometry-3k" \
+  trainer.run_name="geometry-3k-qwen2.5-3b-kl-1e-2" \
   trainer.resume_mode=null \
-  trainer.ckpt_path="$HOME/data/skyrl/ckpts/geometry-3k_ckpt" \
+  trainer.ckpt_path="$HOME/data/skyrl/ckpts/geometry-3k-qwen2.5-3b-kl-1e-2_ckpt" \
   $@
