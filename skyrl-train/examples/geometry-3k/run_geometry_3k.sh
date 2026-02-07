e7df9a071c50bfc6989e6d5286c2e7a908612448
Nithin Chalapathi <nithinc@berkeley.edu>
2026-02-07 01:49:28 +0000

    run script

--- skyrl-train/examples/geometry-3k/run_geometry_3k.sh
+++ skyrl-train/examples/geometry-3k/run_geometry_3k.sh
@@ -44,7 +44,7 @@ uv run --isolated --extra $INFERENCE_BACKEND -m skyrl_train.entrypoints.main_bas
   generator.inference_engine_tensor_parallel_size=1 \
   trainer.epochs=20 \
   trainer.eval_batch_size=512 \
-  trainer.eval_before_train=false \
+  trainer.eval_before_train=true \
   trainer.eval_interval=5 \
   trainer.update_epochs_per_batch=1 \
   trainer.train_batch_size=64 \
