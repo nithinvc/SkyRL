a1ded101f044a3729813de372b3c8ad47d0329f3
Nithin Chalapathi <nithin.ch10@gmail.com>
2026-03-27 17:32:28 -0700

    renderer tests

--- skyrl/train/renderers/image_utils.py
+++ skyrl/train/renderers/image_utils.py
@@ -26,8 +26,10 @@ def get_image_processor(model_name: str) -> ImageProcessor:
 
     from transformers.models.auto.image_processing_auto import AutoImageProcessor
 
-    processor = AutoImageProcessor.from_pretrained(model_name, use_fast=True)
-    assert processor.is_fast, f"Could not load fast image processor for {model_name}"
+    try:
+        processor = AutoImageProcessor.from_pretrained(model_name, use_fast=True)
+    except Exception:
+        processor = AutoImageProcessor.from_pretrained(model_name, use_fast=False)
     return processor
 
 
