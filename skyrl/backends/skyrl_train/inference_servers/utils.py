864cbfc6db9dfb97f2cf4320aa8e67a8b983e456
Nithin Chalapathi <nithin.ch10@gmail.com>
2026-03-31 16:50:12 -0700

    fix generation cache arg

--- skyrl/backends/skyrl_train/inference_servers/utils.py
+++ skyrl/backends/skyrl_train/inference_servers/utils.py
@@ -45,6 +45,7 @@ def build_vllm_cli_args(cfg: SkyRLTrainConfig) -> Namespace:
             if cfg.generator.inference_engine.served_model_name
             else None
         ),
+        disable_mm_preprocessor_cache=True,
     )
 
     # Resolve engine_init_kwargs once for use below
